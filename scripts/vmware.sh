5e3d35772e738bd0f4582f4b587aa6866b789175
juliusblank <dev@juliusblank.de>
2015-10-28 03:52:00 -0400

    Fix provisioner. Fix typo for VMware tools

--- scripts/vmware.sh
+++ scripts/vmware.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# Only run for vmware
+# Bail if we are not running inside VMWare.
 if [[ `facter virtual` != "vmware" ]]; then
-  exit 0
+    exit 0
 fi
 
+# Install the VMWare Tools from a linux ISO.
+
+#wget http://192.168.0.185/linux.iso -P /tmp
 mkdir -p /mnt/vmware
 mount -o loop /home/vagrant/linux.iso /mnt/vmware
 
 cd /tmp
-tar xzf /mnt/vmware/VMWareTools-*.tar.gz
+tar xzf /mnt/vmware/VMwareTools-*.tar.gz
 
-ummount /mnt/vmwware
+umount /mnt/vmware
 rm -fr /home/vagrant/linux.iso
 
 /tmp/vmware-tools-distrib/vmware-install.pl -d
 rm -fr /tmp/vmware-tools-distrib
-
